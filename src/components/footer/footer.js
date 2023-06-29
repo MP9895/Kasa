81be1ec15de8ae1b856b095a1fe24e45908b8f43
Martin <martinperoni95@gmail.com>
2023-06-30 00:08:11 +0200

    Modification du footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,14 +1,21 @@
-import './footer.scss'
-import logo from '../../assets/logo_footer.png'
 import React from 'react';
+import PropTypes from 'prop-types';
+import logo from '../../assets/logo_footer.png';
+import './footer.scss';
 
-function Footer() {
-	return (
-		<footer className='footer'>
-			<img src={logo} alt="Kasa - agence de location d'appartements entre particuliers" />
-			<p className='footer_copyright'>© 2020 Kasa. All rights reserved</p>
-		</footer>
-	)
+function Footer({ fixedFooter }) {
+  const footerClassName = fixedFooter ? 'fixed-footer' : '';
+
+  return (
+    <footer className={`footer ${footerClassName}`}>
+      <img src={logo} alt="Kasa - agence de location d'appartements entre particuliers" />
+      <p className='footer_copyright'>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
 }
+
+Footer.propTypes = {
+  fixedFooter: PropTypes.bool,
+};
 
 export default Footer;
--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -43,7 +43,7 @@ function About() {
 					)}
 				)}
 			</main>
-			<Footer />
+			<Footer fixedFooter={true} />
 		</div>
 	)
 }
